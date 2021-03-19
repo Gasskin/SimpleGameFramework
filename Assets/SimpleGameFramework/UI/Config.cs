bb7a69d032a61e0b352b9193b97dd5209929321a
旧亚楠\Gascoigne <415753928@qq.com>
2021-03-20 01:02:06 +0800

    UI01

---
+++ Assets/SimpleGameFramework/UI/Config.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UIType
+{
+    /// <summary>
+    /// 固定类型，一般作为背景
+    /// </summary>
+    Fixed,
+    /// <summary>
+    /// 普通窗口，一般窗口都用这个
+    /// </summary>
+    Normal,
+    /// <summary>
+    /// 弹窗，可以在某些窗口上弹出的窗口
+    /// </summary>
+    PopUp
+}
---
+++ Assets/SimpleGameFramework/UI/UIBase.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class UIBase : MonoBehaviour
+{
+    #region Public
+
+    /// 窗口类型，默认是Normal
+    public UIType type = UIType.Normal;
+
+    #endregion
+    
+    #region MonoBehaviour
+
+    private void Awake()
+    {
+        Load();
+    }
+
+    private void OnDestroy()
+    {
+        UnLoad();
+    }
+
+    #endregion
+    
+    #region 生命周期
+
+    /// <summary>
+    /// 加载UI，类似于Awake()，多用于初始化
+    /// </summary>
+    public abstract void Load();
+
+    /// <summary>
+    /// 卸载UI，类似于OnDestroy()，多用于卸载
+    /// </summary>
+    public abstract void UnLoad();
+
+    /// <summary>
+    /// 会在UI管理器中被统一调用，用于更新
+    /// </summary>
+    public virtual void OnUpdate()
+    {
+        
+    }
+
+    /// <summary>
+    /// 打开UI
+    /// </summary>
+    public virtual void Show()
+    {
+        
+    }
+
+    /// <summary>
+    /// 关闭UI
+    /// </summary>
+    public virtual void Close()
+    {
+        
+    }
+
+    /// <summary>
+    /// 冻结UI
+    /// </summary>
+    public virtual void Freeze()
+    {
+        
+    }
+
+    /// <summary>
+    /// 解冻UI
+    /// </summary>
+    public virtual void UnFreeze()
+    {
+        
+    }
+
+    #endregion
+}
