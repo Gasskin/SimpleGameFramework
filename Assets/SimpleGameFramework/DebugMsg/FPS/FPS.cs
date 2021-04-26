6b67432263f1cd337d153015c3034609683f812e
旧亚楠\Gascoigne <415753928@qq.com>
2021-04-26 14:14:32 +0800

    1

---
+++ Assets/SimpleGameFramework/DebugMsg/FPS/FPS.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class FPS : MonoBehaviour
+{
+    private int count = 0;
+    private float time = 0;
+    private bool flag = false;
+    private int fps = 0;
+    
+    private void Update()
+    {
+        count++;
+        time += Time.deltaTime;
+        if (time > 1.0f)
+        {
+            fps = count;
+            count = 0;
+            time = 0;
+        }
+            
+    }
+
+    private void OnGUI()
+    {
+        GUI.color = Color.green;
+        GUI.skin.label.fontSize = 50;
+        GUI.Label(new Rect(0, 0, 200, 100), fps.ToString());
+        GUI.color= Color.black;
+    }
+}
